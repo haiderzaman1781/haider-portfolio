227a354616fcd98b269bda7526fa9eefd381cc9e
haiderzaman1781 <haiderzaman1781@gmail.com>
2024-12-18 22:01:43 +0500

    ok

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,7 +1,7 @@
 import React, {  useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import pic from "../assest/Profile.jpg"
+import pic from "../assest//Profile.png"
 
 const Aboutme = () => {
   useEffect(() => {
